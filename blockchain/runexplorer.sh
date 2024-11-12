bcd5bdd07e9dd85b7d6943afdaae0b00288cd25e
ubuntu <ubuntu@localhost.localdomain>
2024-11-12 18:27:32 +0800

    change some shell scripts

--- blockchain/runexplorer.sh
+++ blockchain/runexplorer.sh
@@ -4,6 +4,6 @@ export EXPLORER_PROFILE_DIR_PATH=./connection-profile
 export FABRIC_CRYPTO_PATH=./organizations
 export PORT=3030
 cp -r organizations ./explorer/
-chown -R shh organizations
+chown -R $USER organizations
 mv ./explorer/organizations/store.carlife.com/users/Admin1@store.carlife.com/msp/keystore/*sk ./explorer/organizations/store.carlife.com/users/Admin1@store.carlife.com/msp/keystore/key.pem
 docker-compose -f explorer/docker-compose.yaml up -d
